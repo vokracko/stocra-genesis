7c137a40f5ba3ac1e8d9f9b8c44a4f7d988539e2
Lukáš Vokráčko <lukas@b2c2.com>
2021-11-18 07:05:15 +0100

    Define choices for blockchain name

--- genesis/constants.py
+++ genesis/constants.py
@@ -1,6 +1,11 @@
 from enum import Enum
+from typing import List, Type
 
 
 class BlockchainName(Enum):
     BITCOIN = "bitcoin"
     ETHEREUM = "ethereum"
+
+    @classmethod
+    def choices(self, cls: Type) -> List:
+        return [(choice.name, choice.value) for choice in cls]
