da222926f53582fc66cf0e5272bd58aa718fd028
Lukáš Vokráčko <lukas@b2c2.com>
2022-02-13 10:04:42 +0100

    Define bitcoin node hostname

--- cli.py
+++ cli.py
@@ -4,4 +4,4 @@ from fire import Fire
 from genesis.blockchain.factory import ADAPTERS
 
 if __name__ == "__main__":
-    Fire({blockchain.value: adapter("http://172.21.0.2:8332") for blockchain, adapter in ADAPTERS.items()})
+    Fire({blockchain.value: adapter("http://nodes-bitcoin-1:8332") for blockchain, adapter in ADAPTERS.items()})
