f4f3ef1f56004640a9629ab67c77761682ac836a
Lukáš Vokráčko <lukas@b2c2.com>
2022-03-01 17:38:12 +0100

    Fix adapter fixture

--- genesis/blockchain/bitcoin/tests/conftest.py
+++ genesis/blockchain/bitcoin/tests/conftest.py
@@ -6,11 +6,12 @@ from genesis.blockchain.bitcoin.adapter import BitcoinNodeAdapter
 from genesis.blockchain.factory import NodeAdapterFactory
 from genesis.constants import BlockchainName
 
-EXPECTED_HEADERS = dict(Authorization="Basic cnBjdXNlcjpycGNwYXNzd29yZA==")
+NODE_TOKEN = "token"
 NODE_URL = "http://127.0.0.1:666/"
+EXPECTED_HEADERS = dict(Authorization=f"Basic {NODE_TOKEN}")
 
 
 @pytest.fixture
 def adapter() -> BitcoinNodeAdapter:
-    client = NodeAdapterFactory.get_client(BlockchainName.BITCOIN, url=NODE_URL)
+    client = NodeAdapterFactory.get_client(BlockchainName.BITCOIN, url=NODE_URL, token=NODE_TOKEN)
     return cast(BitcoinNodeAdapter, client)
