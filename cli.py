d167ce37578f2c84884fdc7b23b131d5161628c5
Lukáš Vokráčko <lukas@b2c2.com>
2022-03-03 18:20:45 +0100

    Fix dogecoin get_block_by_hash

--- cli.py
+++ cli.py
@@ -3,5 +3,7 @@ from fire import Fire
 
 from genesis.blockchain.factory import ADAPTERS
 
+TOKEN = "d2VmaWplcmtsajprbDIzMDk0ODIzMDlmbV9hd2RjJV5AJQ=="
+
 if __name__ == "__main__":
-    Fire({blockchain.value: adapter("http://nodes-bitcoin-1:8332") for blockchain, adapter in ADAPTERS.items()})
+    Fire({blockchain.value: adapter("http://dogecoin-node-1:8000", TOKEN) for blockchain, adapter in ADAPTERS.items()})
--- genesis/blockchain/dogecoin/adapter.py
+++ genesis/blockchain/dogecoin/adapter.py
@@ -6,3 +6,10 @@ from genesis.constants import BlockchainName
 
 class DogecoinNodeAdapter(LitecoinNodeAdapter):
     BLOCKCHAIN: ClassVar[BlockchainName] = BlockchainName.DOGECOIN
+
+    async def get_block_by_hash(self, block_hash: str, *, include_transactions: bool) -> dict:
+        data = dict(
+            method="getblock",
+            params=[block_hash, True],
+        )
+        return await self.post(data)
