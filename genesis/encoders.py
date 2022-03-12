354e0a1e9adc287e168cdc1ccec0142332edc557
Lukáš Vokráčko <lukas@b2c2.com>
2022-03-12 11:59:15 +0100

    Fast orjson serializer

--- genesis/encoders.py
+++ genesis/encoders.py
@@ -2,9 +2,15 @@ import decimal
 import json
 from typing import Any
 
+import orjson
+
 
 class DecimalEncoder(json.JSONEncoder):
     def default(self, o: Any) -> Any:
         if isinstance(o, decimal.Decimal):
             return str(o)
         return super().default(o)
+
+
+def fast_serializer(obj: dict) -> str:
+    return orjson.dumps(obj).decode()  # pylint:disable=no-member
