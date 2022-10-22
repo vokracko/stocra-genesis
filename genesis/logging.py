639b2c31ea10d2e1a62f6d118dafa69600d3a3f2
Lukáš Vokráčko <lukas@vokracko.cz>
2022-10-22 20:04:46 +0200

    Logging: fix open encoding

--- genesis/logging.py
+++ genesis/logging.py
@@ -2,7 +2,7 @@ import logging.config
 
 import yaml
 
-with open("logging_config.yaml") as fd:
+with open("logging_config.yaml", encoding="utf-8") as fd:
     logging_config = yaml.safe_load(fd)
 
 logging.config.dictConfig(logging_config)
