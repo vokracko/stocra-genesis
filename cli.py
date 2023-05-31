05d765ea3f5dd0a78746425ec875a1d777aff115
Lukáš Vokráčko <lukas@vokracko.cz>
2023-05-31 14:06:49 +0200

    Remove cli.py

--- cli.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from fire import Fire
-
-from genesis.blockchain.factory import ADAPTERS
-
-TOKEN = "d2VmaWplcmtsajprbDIzMDk0ODIzMDlmbV9hd2RjJV5AJQ=="
-
-if __name__ == "__main__":
-    Fire({blockchain.value: adapter("http://dogecoin-node-1:8000", TOKEN) for blockchain, adapter in ADAPTERS.items()})
